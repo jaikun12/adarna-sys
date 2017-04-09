0e634c818ea84130478d90913bd6483e0fb0b275
jaikun <jaicelim@gmail.com>
2017-04-09 18:56:08 +0800

    first push

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<title>Adarna Dashboard</title>
+	</head>
+	<body>
+		<nav class="main-nav">
+			<img class="logo" src="images/logo.png">
+
+		</nav>
+
+		<div id="appointments" class="main-card">
+			<h3>Appointments</h3>
+			<table class="table table-hover">
+				<thead>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Mobile Number</th>
+					<th>Preferred Date</th>
+					<th>Message</th>
+					<th></th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Jonathan Cabug</td>
+						<td>j.cabug@gmail.com</td>
+						<td>09178890923</td>
+						<td>7/12/2017</td>
+						<td>Sample Message</td>
+						<td><button>Noted</button></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+		<div id="reservation" class="main-card">
+			<h3>Reservations</h3>
+			<table class="table table-hover">
+				<thead>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Mobile Number</th>
+					<th>Destination</th>
+					<th>Check In</th>
+					<th>Check Out</th>
+					<th>Adults</th>
+					<th>Children</th>
+					<th></th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Jonathan Cabug</td>
+						<td>j.cabug@gmail.com</td>
+						<td>09178890923</td>
+						<td>South Africa</td>
+						<td>7/12/2017</td>
+						<td>7/24/2017</td>
+						<td>5</td>
+						<td>3</td>
+						<td><button>Noted</button></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+		<div id="quotations" class="main-card">
+			<h3>Email Quotations</h3>
+			<table class="table table-hover">
+				<thead>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Mobile Number</th>
+					<th>Destination</th>
+					<th></th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Jonathan Cabug</td>
+						<td>j.cabug@gmail.com</td>
+						<td>09178890923</td>
+						<td>South Africa</td>
+						<td><button>Noted</button></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+		<div id="requests" class="main-card">
+			<h3>Call Requests</h3>
+			<table class="table table-hover">
+				<thead>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Mobile Number</th>
+					<th>Destination</th>
+					<th></th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Jonathan Cabug</td>
+						<td>j.cabug@gmail.com</td>
+						<td>09178890923</td>
+						<td>South Africa</td>
+						<td><button>Noted</button></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+	</body>
+</html>
